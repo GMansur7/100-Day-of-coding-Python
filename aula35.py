d15b54b5791947fb829ae6a8d50cb19c8286cc52
Gustavo Mansur de Oliveira <78883690+GMansur7@users.noreply.github.com>
2021-06-20 21:21:52 -0300

    Add files via upload

--- aula35.py
+++ aula35.py
@@ -4,29 +4,31 @@ name2 = input("\nWhats is their name?\n")
 
 #variable.lower() Is case sensitive
 #variable.count()
-# campare whit TRUE LOVE
-score1 = 0
-score2 = 0
+# compare whit TRUE LOVE
 
-score1 += name1.lower().count("t")
-score1 += name1.lower().count("r")
-score1 += name1.lower().count("u")
-score1 += name1.lower().count("e")
+#score1 = 0
+#score2 = 0
+couple = name1 + name2
 
-score1 += name2.lower().count("t")
-score1 += name2.lower().count("r")
-score1 += name2.lower().count("u")
-score1 += name2.lower().count("e")
+score1 =  couple.lower().count("t")
+score1 += couple.lower().count("r")
+score1 += couple.lower().count("u")
+score1 += couple.lower().count("e")
 
-score2 += name1.lower().count("l")
-score2 += name1.lower().count("o")
-score2 += name1.lower().count("v")
-score2 += name1.lower().count("e")
+#score1 += name2.lower().count("t")
+#score1 += name2.lower().count("r")
+#score1 += name2.lower().count("u")
+#score1 += name2.lower().count("e")
 
-score2 += name2.lower().count("l")
-score2 += name2.lower().count("o")
-score2 += name2.lower().count("v")
-score2 += name2.lower().count("e")
+#score2 =  name1.lower().count("l")
+#score2 += name1.lower().count("o")
+#score2 += name1.lower().count("v")
+#score2 += name1.lower().count("e")
+
+score2 =  couple.lower().count("l")
+score2 += couple.lower().count("o")
+score2 += couple.lower().count("v")
+score2 += couple.lower().count("e")
 
 result = str(score1) + str(score2)
 
