4f171f1eec904619fcb6ddbcf528584c3de8c115
Gustavo Mansur de Oliveira <78883690+GMansur7@users.noreply.github.com>
2021-06-26 23:50:46 -0300

    Add files via upload

--- aula45.py
+++ aula45.py
@@ -1,48 +1,72 @@
 import random
 
 while 1:
-	text = (input("what do you chose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+	choice = int(input("what do you chose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-	choise = int(text)
-	vector = ["Pedra", "Papel", "Tesoura"]
+	rock = '''
+	    _______
+	---'   ____)
+	      (_____)
+	      (_____)
+ 	      (____)
+	---.__(___)
+	'''
+	paper = '''
+	    _______
+	---'   ____)____
+	          ______)
+	          _______)
+	         _______)
+	---.__________)
+	'''
+	scissors = '''
+	    _______
+	---'   ____)____
+	          ______)
+	       __________)
+	      (____)
+	---.__(___)
+	'''
 
-	pc_choise = random.randint(0, 2)
+	vector = [rock, paper, scissors]
+
+	pc_choice = random.randint(0, 2)
 	
-#	print(f"vc escolheu {vector[choise]}")
+#	print(f"You choice {vector[choice]}")
 
-	if choise >= 3 or choise < 0:
-#	if 0 > choise >= 3:
-		print("entrada invalida, vc perdeu")
-	elif choise == pc_choise:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
-		print("Empate ")
-	elif choise == 0 and pc_choise == 1:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	if choice >= 3 or choice < 0:
+#	if 0 > choice >= 3: # (0 > choice) >= 3
+		print("Invalid entry, you lose")
+	elif choice == pc_choice:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
+		print("It's a draw!")
+	elif choice == 0 and pc_choice == 1:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You loose")
-	elif choise == 1 and pc_choise == 2:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	elif choice == 1 and pc_choice == 2:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You loose")
-	elif choise == 2 and pc_choise == 0:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	elif choice == 2 and pc_choice == 0:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You loose")
-	elif choise == 0 and pc_choise == 2:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	elif choice == 0 and pc_choice == 2:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You Win!")
-	elif choise == 1 and pc_choise == 0:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	elif choice == 1 and pc_choice == 0:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You Win!")
-	elif choise == 2 and pc_choise == 1:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	elif choice == 2 and pc_choice == 1:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You Win!")
-	elif choise == 1 and pc_choise == 0:
-		print(f"vc escolheu {vector[choise]}")
-		print(f"Pc choise {vector[pc_choise]}")
+	elif choice == 1 and pc_choice == 0:
+		print(f"You choice {vector[choice]}")
+		print(f"Pc choice {vector[pc_choice]}")
 		print("You Win!")
 			
