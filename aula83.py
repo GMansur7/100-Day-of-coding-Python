c2d16260ed973abaa041d18ad43c55c548d138c8
Gustavo Mansur de Oliveira <78883690+GMansur7@users.noreply.github.com>
2021-07-09 12:41:16 -0300

    Add files via upload

--- aula83.py
+++ aula83.py
@@ -5,7 +5,45 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text=text, shift=shift, alphabet=alphabet):
+		cipher_text = []
+		cipher_index = []
+		for letter in text:
+			position = alphabet.index(letter)
+			if ((position + shift) <= 25):
+				cipher_index.append(position + shift)
+#				print (cipher_index)
+			else:		
+				cipher_index.append(position + shift - 26)	
+		print(cipher_index) 
+		for index in cipher_index:
+			cipher_text.append(alphabet[index])
+		print( "".join(cipher_text))
+###############################		
+def decrypt(text=text, shift=shift,alphabet=alphabet):
+	cipher_text = []
+	cipher_index = []
+	mensagem = []
+	for letter in text:
+		cipher_text.append(letter)
+		position = cipher_text.index(letter)
+		print (position)
+		if (position - shift) > 0:	
+			cipher_index.append(position - shift)
+		else:
+			cipher_index.append(position - shift)	
+	print(cipher_text)
+	print(cipher_index)
+	for index in cipher_index:
+		mensagem.append(alphabet[index])
+	print("".join(mensagem))
 
+if direction == "encode":
+	encrypt ()
+elif direction == "decode":
+    decrypt()
+else:
+    print("Invalid function")
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
     #plain_text = "hello"
