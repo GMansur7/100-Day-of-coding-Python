6b3cdfb5ca952133f5740b91f3d2464c280b1fb2
Gustavo Mansur de Oliveira <78883690+GMansur7@users.noreply.github.com>
2021-09-14 23:54:29 -0300

    Add files via upload

--- aula31.py
+++ aula31.py
@@ -1,13 +1,11 @@
-#ano bissexto 
-loop = 0
-while loop <= 9:
-	loop += 1
-	year = int(input("whit year do you want to check?"))
-	flag =0
-	if year % 4 == 0:
-		if year % 100 != 0:
-			if year % 400 != 0:
-				print("The year is leap")
-				flag = 1 
-	if flag == 0:
-		print("The year is not leap")
+year = int(input("whit year do you want to check?"))
+if year % 4 == 0:
+	if year % 100 == 0:
+		if year % 400 == 0:
+			print("The year is leap")
+		else:
+			print("The year is not leap")
+	else:
+		print("The year is leap")
+else:
+	print("The year is not leap")
